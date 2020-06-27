8938159485ca737acbe6b7ca474f4bbce1431254
kinsprite <qxqs11@126.com>
2020-06-27 08:39:54 +0800

    init repo

---
+++ README.md
@@ -0,0 +1,14 @@
+
+# React Micro Frontend Polyfill
+
+React polyfill for modern browsers and IE9/IE11.
+
+## Features
+
+* Register `Micro Frontend` apps on start up or later.
+* Load `Micro Frontend` apps dynamically.
+* Share common libraries for all `Micro Frontend` apps.
+* A global redux store can be used for states which cross multi `Micro Frontend` apps.
+
+## Example
+See: https://micro.qinzhiqiang.cn
---
+++ scripts/build.js
@@ -0,0 +1,24 @@
+const scripts = require('react-micro-frontend-scripts');
+
+function build() {
+  // --- ENV for 'production' only ---
+  process.env.PUBLIC_ROOT_URL = '/';
+  process.env.GENERATE_SOURCEMAP = 'false';
+  // process.env.INLINE_RUNTIME_CHUNK = 'true';
+  // process.env.MINIMIZE_IN_PRODUCTION = 'true';
+
+  // --- ENV for ALL ---
+  // process.env.IMAGE_INLINE_SIZE_LIMIT = '1000';
+  // process.env.REACT_MICRO_FRONTEND_SHORT = 'rmf';
+  process.env.SPLIT_CHUNKS = 'false';
+  process.env.RUNTIME_CHUNK = 'false';
+
+  scripts.runWebpack(scripts.envProduction, (config) => {
+    const newConfig = scripts.helper.webpackConfigCallback(config);
+    newConfig.entry['polyfill-ie9'] = [scripts.resolvePath('src/polyfill-ie9')];
+    newConfig.entry['polyfill-ie11'] = [scripts.resolvePath('src/polyfill-ie11')];
+    return newConfig;
+  });
+}
+
+build();
